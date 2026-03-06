ea877ad009aad7609d510a6c033df7a5b92d651f
MILKAA <kameel.2011@icloud.com>
2026-03-06 20:48:59 +0100

    Fix the LLM.py

--- final/LLm/llm.py
+++ final/LLm/llm.py
@@ -7,7 +7,7 @@ def chat(message, historique=[]):
         "http://localhost:1234/v1/chat/completions",
         json={
             "model": "local-model",
-            "messages": [
+            "messages": " MIKO AI : Hey ! Je peux t'aider sur quoi ?" [
                 {"role": "system", "content": "Tu es MIKO AI, un assistant intelligent. Tu est là, pour répondre et aider l'utilisateur, sois précis, gentil avec un ton d'humour. Si tu ne sais pas quoi répondre notifie-le à l'utilisateur."}
             ] + historique
         }
@@ -19,13 +19,13 @@ def chat(message, historique=[]):
     return reponse_texte
 
 # Boucle de conversation
-print("MIKO AI — tape 'quitter' pour arrêter\n")
+print("MIKO AI — tape 'mk exit' pour arrêter\n")
 historique = []
 
 while True:
     user_input = input("Toi : ")
-    if user_input.lower() == "quitter":
+    if user_input.lower() == "mk exit":
         break
     
     reponse = chat(user_input, historique)
-    print(f"MIKO : {reponse}\n")
+    print(f"MIKO AI : {reponse}\n")
