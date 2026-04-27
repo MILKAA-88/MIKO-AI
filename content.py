c2e60cd9fa82b90133328770e5c4348ea6d1d825
MILKAA <kameel.2011@icloud.com>
2026-04-27 17:19:27 +0200

    Fix: show content.py

--- content.py
+++ content.py
@@ -1,17 +1,22 @@
 import cv2
 import tkinter as tk
 from PIL import Image, ImageTk
+import os
 
-videos = ["/video/Miko-ai1.mp4", "/video/Miko-ai2-1.mp4", "/video/Miko-ai2-2.mp4"]
-index = 0
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+index = 0 
+
+videos = [
+    os.path.join(BASE_DIR, "video", "Miko-ai1.mp4"),
+    os.path.join(BASE_DIR, "video", "Miko-ai2-1.mp4"),
+    os.path.join(BASE_DIR, "video", "Miko-ai2-2.mp4")
+]
 
 def show_content():
     global index
-
     root = tk.Tk()
     root.configure(bg="black")
     root.geometry("1080x720")
-
     label = tk.Label(root, bg="black")
     label.pack(expand=True)
 
@@ -22,7 +27,6 @@ def show_content():
         def next_frame():
             global index
             ret, frame = cap.read()
-
             if ret:
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 img = ImageTk.PhotoImage(Image.fromarray(frame))
@@ -36,5 +40,8 @@ def show_content():
 
         next_frame()
 
-    play_video()
-    root.mainloop()
+    play_video()       
+    root.mainloop()    
+
+if __name__ == "__main__":
+    show_content()     
--- main.py
+++ main.py
@@ -18,3 +18,4 @@ if __name__ == "__main__":
 
     thread.join()
     show_updater()
+    show_content() 
